e64191abe9ce0fb71f3b07e03dcac52ba3d6dabf
Houra <hu.a.mashaee@gmail.com>
2021-04-12 18:49:14 +0200

    modification

--- all-ears-front/src/App.js
+++ all-ears-front/src/App.js
@@ -3,14 +3,13 @@ import "./App.css";
 import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/main/NavBar.jsx";
-import ShowDetails from "../src/components/podPage/ShowDetails"
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
       {/* <Signup /> */}
-      <ShowDetails></ShowDetails>
+      {/* <ShowDetails></ShowDetails> */}
     </div>
   );
 }
