f8d40c79705a31662ea89a90de11d511f6424e05
Allan <jdll.allan@gmail.com>
2021-04-15 10:27:33 +0200

    update fixtures content

--- all-ears-back/models/fixtures.js
+++ all-ears-back/models/fixtures.js
@@ -22,19 +22,27 @@ async function createUser() {
 async function createLikes() {
   try {
     await PodcastsLikes.bulkCreate([
-      { fk_user_id: 1, podcast_id: "7de578c61c5b43788af01b71dee66fd2" },
       {
         fk_user_id: 1,
-        podcast_id: "b300121e4a224fc7be5fe7fcb322a4e9",
+        podcast_id: "e8b52010c95b4ab18b34dc9a2f5902c9"
       },
       {
         fk_user_id: 1,
-        podcast_id: "4e2bd6fab99a4e268b4470bfd4aca599",
+        podcast_id: "be8917211b0045fd96b8ada0f0e4b79d",
       },
       {
         fk_user_id: 1,
-        podcast_id: "68660f76903348f39162cf53e5d2ec82",
+        podcast_id: "dbcc884f28d34d6e8bd06f4e4af249aa",
       },
+      {
+        fk_user_id: 1,
+        podcast_id: "83cccaf5913b4b7491c9c7c03a887bb1",
+      },
+      {
+        fk_user_id: 1,
+        podcast_id: "1e802717fb6548d98263bd9d58facbba",
+      },
+
     ]);
     console.log("Likes podcast for Jane created!");
   } catch (err) {
